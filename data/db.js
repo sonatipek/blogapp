bf63d07a7824482bfacd83e6f0ce479f891d4638
Sonat Saygın İPEK <sonatipek55@gmail.com>
2023-07-24 20:29:20 +0300

    added gitignore

--- data/db.js
+++ data/db.js
@@ -1,16 +1,3 @@
 const mysql = require('mysql2');
 const config = require('./config');
 
-// DB Connection
-const connection = mysql.createConnection(config.db);
-
-connection.connect((err) => {
-    if (err) {
-        return console.log(err);
-    }
-    console.log("MySQL connection is done");
-    
-});
-
-
-module.exports=connection.promise();
