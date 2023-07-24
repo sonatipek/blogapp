42262e5d55db61de2b9fc789d0b48dfa200796e2
Sonat Saygın İPEK <sonatipek55@gmail.com>
2023-07-24 20:47:53 +0300

    sequelize ORM added project

--- data/db.js
+++ data/db.js
@@ -1,3 +1,22 @@
 const mysql = require('mysql2');
 const config = require('./config');
 
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    dialect: "mysql",
+    host: process.env.DB_HOST
+});
+
+async function connect() {
+    try {
+        await sequelize.authenticate();
+        console.log("MySQL bağlantısı başarılı");
+    } catch (err) {
+        console.error(`MySQL bağlantısında hata! Hata:${err}`)
+    }
+}
+
+connect();
+
+module.exports=sequelize;
