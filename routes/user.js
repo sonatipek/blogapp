d420fa888fb3fb0b8922a75893fba03c921e6beb
Sonat <80075444+sonatipek@users.noreply.github.com>
2023-07-24 20:04:36 +0300

    admin dasboard deleted

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Requirements
 const express = require('express');
 const db = require('../data/db');
@@ -75,4 +76,83 @@ router.use("/",async (req, res) => {
     
 });
 
+=======
+// Requirements
+const express = require('express');
+const db = require('../data/db');
+
+// Create Router
+const router = express.Router();
+
+// Blogs by Category
+router.use('/blogs/category/:categoryid', async (req, res) => {
+    try {
+        const categoryid = req.params.categoryid;
+        const [blogs, ] = await db.execute("select * from blogs where category = ?", [categoryid]);
+        const [categories, ] = await db.execute('select * from category')
+        
+        
+
+        res.render("pages/category-list", {blogs: blogs, categories: categories, selectedCategory: categoryid}) 
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+// Blog Detail
+router.use("/blogs/:blogid",async (req, res) => {
+    try {
+        const [blogs, ] = await db.execute('select * from blogs');
+        const [categories, ] = await db.execute('select * from category')
+     
+        
+        if (req.params.blogid > 0) {
+            res.render('pages/blog-detail', {blogs: blogs, categories: categories, blogid: req.params.blogid});
+        }else{
+            res.render("pages/errors/404.ejs");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+// All Blogs
+router.use("/blogs", async (req, res) => {
+
+    try {
+        const [blogs, ] = await db.execute('select * from blogs');
+        const [categories, ] = await db.execute('select * from category')
+       
+    
+        res.render('pages/blog', {
+            blogs: blogs,
+            categories: categories
+        })
+        
+    } catch (err) {
+        console.log(err);
+    }
+
+});
+
+// Homepage
+router.use("/",async (req, res) => {
+    try {
+        const [blogs, ] = await db.execute('select * from blogs');
+        const [categories, ] = await db.execute('select * from category')
+        
+        res.render('pages/index', {
+            blogs:blogs,
+            categories: categories
+        })
+        
+    } catch (err) {
+        console.log(err);
+    }
+
+    
+});
+
+>>>>>>> e1cc4f6cb4e909b8fe7adc3034875226c04e20f2
 module.exports=router;
