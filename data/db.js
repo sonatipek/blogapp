caefbfbe3ad407d9f3a7c732873eb8f5f1f818b9
Sonat Saygın İPEK <sonatipek55@gmail.com>
2023-07-26 19:20:59 +0300

    config file deleted

--- data/db.js
+++ data/db.js
@@ -1,7 +1,5 @@
-const mysql = require('mysql2');
-const config = require('./config');
-
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     dialect: "mysql",
