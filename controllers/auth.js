01a16c5d5aabf98f1b4ad7d59138f3dfc185e251
Sonat Saygın İPEK <sonatipek55@gmail.com>
2023-09-12 12:46:55 +0300

    bcypt lib added. passwords are now encrypted in the database during registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 // Create Models
 const User = require('../models/user');
-
+const bcrypt = require('bcrypt');
 
 exports.getRegister = async (req, res) => {
     try {
@@ -14,13 +14,13 @@ exports.postRegister = async (req, res) => {
     try {
         const name = req.body.user_name,
         email = req.body.user_email,
-        password = req.body.user_password;
+        password = await bcrypt.hash(req.body.user_password, 10);
 
         User.create({
             fullname: name,
             email: email,
             password: password
-        })
+        }) 
 
         res.redirect('auth/login')
     } catch (error) {
