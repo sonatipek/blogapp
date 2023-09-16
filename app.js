7d9581f8d49d38606c050fb093ebcca3c70a7a8a
Sonat Saygın İPEK <sonatipek55@gmail.com>
2023-09-16 14:45:04 +0300

    file editing

--- app.js
+++ app.js
@@ -1,37 +1,62 @@
-// Requirements
+// !----Requirements----!
+// Express 
 const express = require('express');
-const path = require('path');
+// Create expressinstance
+const app = express();
 
+// Cookie Parser & Sessions
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+// Routes Requirements
 const userRoutes = require('./routes/user');
 const adminRoutes = require('./routes/admin');
-const authRoutes = require('./routes/auth')
+const authRoutes = require('./routes/auth');
 
-// Variables
-const PORT = 3000; 
+// Node Modules
+const path = require('path');
 
-// Create instance
-const app = express();
-const cookieParser = require('cookie-parser');
+// Custom Modules
+const sequelize = require('./data/db');
+const dummyData = require('./data/dummy-data');
 
-// Middlewares
-app.use('/libs', express.static(path.join(__dirname, 'node_modules'))); //you can give an alias for static folder if you want
-app.use(express.static(path.join(__dirname, 'public')));
+// Template Engine
 app.set("view engine", 'ejs'); // you can use any template engine
-app.use(express.urlencoded({extended: true}));
-app.use(cookieParser());
 
-// !Routes
-app.use('/admin', adminRoutes); //you can set a default startup path
-app.use('/auth', authRoutes); //you can set a default startup path
-app.use(userRoutes);
-
-
-
+// Modals
 const Category = require('./models/category');
 const Blog = require('./models/blog');
 const User = require('./models/user');
 
-// relations
+// Middlewares
+app.use(express.urlencoded({extended: true}));
+
+app.use(cookieParser());
+app.use(session({
+    secret: "helloWorld",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 //milisecond
+    }
+
+}));
+
+app.use('/libs', express.static(path.join(__dirname, 'node_modules'))); //you can give an alias for static folder if you want
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/admin', adminRoutes); //you can set a default startup path
+app.use('/auth', authRoutes); //you can set a default startup path
+app.use(userRoutes);
+
+// Variables
+const PORT = 3000; 
+// !----Requirements----!
+
+
+
+// * Relations
 // onte to many
 Category.hasMany(Blog, {
     foreignKey:{
@@ -54,15 +79,15 @@ Blog.belongsTo(User, {
 });
 User.hasMany(Blog);
 
-// sync
-const sequelize = require('./data/db');
-const dummyData = require('./data/dummy-data');
-// IIFE
+// IIFE - to data sync
 (async () => {
     await sequelize.sync({alter: true})
     await dummyData();
 })();
 
+
+
+// *Server
 app.listen(PORT, () => {
     console.log("Listening on port " + PORT);
 })
